c8cf0850e26a55f212237462bb23e8e63f1954b6
Shivk23 <shiv@rahabsdaughters.org>
2024-07-05 12:49:44 +0530

    5th commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -6,35 +7,34 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.json());
 
+console.log('Email User:', process.env.EMAIL_USER);
+console.log('Email Pass:', process.env.EMAIL_PASS ? 'Exists' : 'Not Set');
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
 app.post('/webhook', (req, res) => {
     const formData = req.body;
 
-    // Set up the email transport configuration
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    // Configure the email options
     let mailOptions = {
         from: process.env.EMAIL_USER,
-        to: formData.email, // Assuming the form has an 'email' field
+        to: formData.email,
         subject: 'Thank you for signing up!',
         text: `Dear ${formData.name},\n\nThank you for filling out the form. We appreciate your interest and will get back to you soon.\n\nBest Regards,\nRahab's Daughters Team`
     };
 
-    // Send the email
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return console.log(error);
+            console.log('Error occurred:', error.message);
+            return res.status(500).send('Failed to send email');
         }
         console.log('Email sent: ' + info.response);
+        res.status(200).send('Webhook received and email sent');
     });
-
-    res.status(200).send('Webhook received and email sent');
 });
 
 const PORT = process.env.PORT || 3000;
